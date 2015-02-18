b32de975edec119aaa9966a6442c0c32996b1c19
Lateef <latrah2002@gmail.com>
2015-02-18 20:01:25 +0100

    Custom css
    I create a custom css

--- wp-content/themes/childofpilotfish/header.php
+++ wp-content/themes/childofpilotfish/header.php
@@ -1,4 +1,6 @@
+
 <?php
+
 /**
  * Header Template
  *
--- wp-content/themes/childofpilotfish/home.php
+++ wp-content/themes/childofpilotfish/home.php
@@ -12,14 +12,16 @@
 get_header(); ?>
     
 	<div id="featured" class="hidden-phone">
+	
+		<h2 class="featured-subtitle"><?php echo __('A Minimal, Responsive Portfolio Theme','pilotfish'); ?></h2>
+            	<p><?php echo __('I can edit this as child theme home.php in the Edit Panel. Happy Blogging! ','pilotfish'); ?></p>
+				
 	<div id="banner-text" class="span12">     
 		<h1 class="featured-title"><?php echo __('African Languages','pilotfish'); ?></h1>
 	</div>
-		<h2 class="featured-subtitle"><?php echo __('A Minimal, Responsive Portfolio Theme','pilotfish'); ?></h2>
-            	<p><?php echo __('I can edit this as child theme home.php in the Edit Panel. Happy Blogging! ','pilotfish'); ?></p>
         </div><!-- end of #featured -->
              
-	<div class="center"><h2><?php _e('TRADITIONAL AFRICAN LANGUAGES','pilotfish')?></h2></div>
+	<div class="center"><h2><?php _e('AFRICAN TRADITIONAL LANGUAGES','pilotfish')?></h2></div>
 	<hr>
 	
 <?php get_sidebar('home'); ?>
